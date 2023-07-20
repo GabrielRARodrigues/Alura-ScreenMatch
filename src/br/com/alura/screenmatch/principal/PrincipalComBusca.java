c8853b24bfc81d1b01e53e671cba00bf64a1dad5
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-07-20 08:47:10 -0300

    Importando no projeto a biblioteca Gson, para realizar a serialização e desserialização de objetos Json, criando o Record TituloOmdb para representar um filme vindo da nossa API e modficiando a classe Titulo, criando um construtor que recebe uma instância de TituloOmdb

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,11 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -16,10 +22,20 @@ public class PrincipalComBusca {
         String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=5e3471fe";
 
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        System.out.println("Titulo já convertido");
+        System.out.println(meuTitulo);
     }
+
 }
